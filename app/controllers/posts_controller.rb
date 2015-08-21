0d80003183058746a7cfd930aff8ff13d5f8b0d4
robrieba <robrieba@gmail.com>
2015-08-21 13:59:07 -0400

    assingment 31 completed.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.each_with_index do |post, i|
+      if i % 5 == 0
+        post.title = "CENSORED"
+      end
+    end
   end
 
   def show
