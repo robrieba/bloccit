f811f4dcd60f9e05097cd7b57349d79b1f89ba9b
robrieba <robrieba@gmail.com>
2015-08-24 14:03:46 -0400

    checkpoint 34 completed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 include RandomData
 
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+
+topics = Topic.all
 50.times do
   Post.create(
     title: RandomData.random_sentence,
@@ -23,5 +31,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
