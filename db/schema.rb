dae7e87d9c80bd5ddf45acbabb3c5b6cf76c8636
robrieba <robrieba@gmail.com>
2015-08-26 22:15:25 -0400

    assignment 38 completed

--- db/schema.rb
+++ db/schema.rb
@@ -28,18 +28,9 @@ ActiveRecord::Schema.define(version: 20150826101459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
-  endassignment-35-validation
+  end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "topic_id"
-  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
