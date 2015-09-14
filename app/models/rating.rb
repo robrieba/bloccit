4bd72c9fd18bf03dc21f9d4c2edbe2c921da5ea0
robrieba <robrieba@gmail.com>
2015-09-14 14:11:57 -0400

    added a nil string check in update_rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,7 +4,9 @@ class Rating < ActiveRecord::Base
   enum severity: [ :PG, :PG13, :R ]
 
   def self.update_rating(rating_string)
-    Rating.find_or_create_by(severity: rating_string.to_sym)
+    if rating_string != nil
+      Rating.find_or_create_by(severity: rating_string.to_sym)
+    end
   end
 
 end
