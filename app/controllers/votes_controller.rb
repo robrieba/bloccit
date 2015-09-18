00e489c0844c0cf0f7929262a71a7375284d13ee
robrieba <robrieba@gmail.com>
2015-09-18 07:45:59 -0400

    assignment 47 completed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,10 +3,20 @@ class VotesController < ApplicationController
 
   def up_vote
     update_vote(1)
+
+    respond_to do |format|
+      format.html { redirect_to topic_posts_path }
+      format.js
+    end
   end
 
   def down_vote
     update_vote(-1)
+
+    respond_to do |format|
+      format.html { redirect_to topic_posts_path }
+      format.js
+    end
   end
 
   private
@@ -19,11 +29,6 @@ class VotesController < ApplicationController
       @vote.update_attribute(:value, new_value)
     else
       @vote = current_user.votes.create(value: new_value, post: @post)
-    end
-
-    respond_to do |format|
-      format.html
-      format.js
     end
   end
 
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -57,12 +57,12 @@ RSpec.describe CommentsController, type: :controller do
 
      describe "POST create" do
        it "increases the number of comments by 1" do
-         expect{ post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
+         expect{ post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence} }.to change(Comment,:count).by(1)
        end
 
        it "redirects to the post show view" do
-         post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-         expect(response).to redirect_to [my_topic, my_post]
+         post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+         expect(response).to have_http_status(:success)
        end
      end
 
@@ -77,6 +77,8 @@ RSpec.describe CommentsController, type: :controller do
         post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_paragraph}
         expect(response).to have_http_status(:success)
       end
+    end
+  end
 
    context "admin user doing CRUD on a comment they don't own" do
      before do
@@ -90,8 +92,8 @@ RSpec.describe CommentsController, type: :controller do
        end
 
        it "redirects to the post show view" do
-         post :create, post_id: my_post.id, comment: {body: RandomData.random_sentence}
-         expect(response).to redirect_to [my_topic, my_post]
+         post :create, format: :js, post_id: my_post.id, comment: {body: RandomData.random_sentence}
+         expect(response).to have_http_status(:success)
        end
      end
 
