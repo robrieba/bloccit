b3503df68c821bfdae83a040367f460f8d50fb47
robrieba <robrieba@gmail.com>
2015-09-18 11:26:20 -0400

    checkpoint 49 completed

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,11 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
+
 end
